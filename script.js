ecbcd5360acf555176d6ea89bcbda5a7bac4996b
Dan Davies <dandavies.bagillt@gmail.com>
2015-06-15 08:16:59 +0100

    made script more generic

--- script.js
+++ script.js
@@ -6,18 +6,22 @@ DD.Map = function()
       function initialize() {
         var mapOptions = {
             zoom: 14,
-            center: new google.maps.LatLng(53.5641642, -2.4155645),
+            center: new google.maps.LatLng(0, 0),
+            // Add your own lon and lat
             scrollwheel: false,
             styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}],
+            // These make the map greyscale. You can add other styles for different effects
             mapTypeId: google.maps.MapTypeId.ROADMAP,
             scroll:{x:$(window).scrollLeft(),y:$(window).scrollTop()},
         };
 
         var icon = { 
-            url: 'http://www.peppermintsoda.co.uk/artez/site/assets/img/artez-pin.png'
+            url: 'assets/img/pin.png'
+            //insert image path here
         };
 
-        map = new google.maps.Map(document.getElementById('artez_map'),
+        map = new google.maps.Map(document.getElementById('client-map'),
+        // Must match the ID of map div in markup
         mapOptions);
 
         new google.maps.Marker({map:map,icon:icon,position:map.getCenter()});
